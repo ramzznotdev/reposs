78ab7ba2719146f63a9de279cc04f8f0f2554aed
ramzznotdev <ramzznotdev@gmail.com>
2025-10-04 07:39:53 +0700

    Add files via upload

--- database/config.js
+++ database/config.js
@@ -1,1 +1,6 @@
-
+module.exports = {
+  tokens: "8348081021:AAEfmr3jqD4P0Eh-1Gm_yj1XAGwvRZrvj-k",  // Ubah Jadi Token Bot Mu !!!
+  owner: "7514841787", // Ubah Jadi Id Mu !!!
+  port: "2002", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "152.42.226.186" // Ubah Jadi Ip Vps Mu !!!
+};
